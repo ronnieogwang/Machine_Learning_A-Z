14697a7b900e8c2698378332cfbcf08818c48647
ronnie <rogwang123@gmail.com>
2019-12-10 18:57:42 +0300

    writing and reading from files

--- python basics/python basics.py
+++ python basics/python basics.py
@@ -52,7 +52,7 @@ for i in t:
 
 '''Data structures'''
     '''1. Lists'''
-x = [1,2,3,4,5,6] #sams data type
+x = [1,3,9,4,5,6] #sams data type
 #indexing
 print(x[0]) #first element is 1
 print(x[1:4]) #print elements from index 1 to 3, 4 exclusive
@@ -62,3 +62,67 @@ print(x[-2]) #second item from the end
 
 for i, y in enumerate(x): #prints index in i and vale in y
     print(i, y)
+    
+x.append(7)#add at the end of list
+x.clear() #clears entire list
+y = x.copy() #duplicates x 
+x.count()
+x.extend
+x.index
+x.insert(0,6) #inserts value at the named index
+x.pop(0) #deletes from list and returns indexed item
+x.remove(4) #removes named item from list
+x.reverse()#  reverses list
+x.sort()  #sorts in ascending order
+print(x)
+
+'''variable scope'''
+#a variable defined outside a function can be accessed by the fucntion but cannot
+#be modified by the fuction, unless if it is passed as an argument.
+
+'''tuples'''
+#lists vs tuples
+numbers = [1,2,3,4] #list brackets
+
+squares = (1,4,9,16) #tuple parentheses
+num = (1,)#single element tuple, use comma at the end
+
+#A tuple, the data is immutable - cannot change, tuples are more faster to process than lists.
+
+print(dir(numbers)) #dir() mtd, is used to see the fucntions that can be done on a data type
+
+'''Dictionaries'''#doesnt store data in an orderly manner, we can use indexes.
+#
+person = {'name':'Max','Age': 18} #definition
+person['name'] #indexing
+person['height'] = 162 #adding a new key value pair
+print(person)
+
+
+'''Writing to files in python'''
+import os
+text = 'I am writing this to python!!' #data
+ 
+file = open('test.txt', 'w') #open file or create it
+file.write(text)
+file.close()
+print('Finished')
+
+#In write mode, pythin erases all existing data and adds new data
+#To avid erasing use append.
+
+'''Reading from files'''
+file = open('test.txt', 'r') #open file or create it
+text = file.read()
+file.close()
+print('Finished')
+print(text)
+
+
+
+
+
+
+
+
+
