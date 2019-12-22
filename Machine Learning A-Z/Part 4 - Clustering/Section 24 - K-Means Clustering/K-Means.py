4d3624479cd8496d12082cca696477a4b9ae7a3f
ronnie <rogwang123@gmail.com>
2019-12-22 17:54:13 +0300

    K-Means clustering algorithm

---
+++ Machine Learning A-Z/Part 4 - Clustering/Section 24 - K-Means Clustering/K-Means.py
@@ -0,0 +1,55 @@
+'''How K-Means works
+    Select K clusters
+    Select K ponts which will be centroids, (not necessarily from the dataset)
+    Assign each data point to the closest centroid, form K -clusters
+    compute new centroid of each cluster
+    reassign each point to the closest cluster, if no reassignment, stop.
+    
+ Random selection trap of centroids
+ We use K-Means++ to select rthe correct centroids 
+ 
+ Number of clusters
+ Use WCSS to selct an optimal number of clusters in the dataset. Plot WCSS against
+ number of clusters. The number of clusters at the 'elbow' of the curve is the optimal
+ number of clusters.
+'''
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:,[3,4]].values
+
+# Using the elbow method to find the optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+#from this dataset, the elbow is at 5 clusters
+# Fitting K-Means to the dataset
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualising the clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
